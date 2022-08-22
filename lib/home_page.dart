7d47a33b99e233f357cf85df4d92c5cd957e46e2
zamama-zaman <zamamazaman@gmail.com>
2022-08-22 23:16:00 +0500

    Updated Code

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,15 +1,35 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:sports_channel_app/list_of_m3u.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Sports Channel Home Page"),
+      body: SafeArea(
+        child: Column(
+          children: [
+            ...List.generate(
+              listM3u.length,
+              (index) {
+                return Card(
+                  child: ListTile(
+                    title: Text(listM3u[index].name!),
+                    leading: CircleAvatar(),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/list_of_m3u.dart
@@ -0,0 +1,12 @@
+class ListM3u {
+  final String? name;
+  final String? image;
+
+  ListM3u({this.name, this.image});
+}
+
+List<ListM3u> listM3u = [
+  ListM3u(name: "Name 1", image: "image"),
+  ListM3u(name: "Name 2", image: "image"),
+  ListM3u(name: "Name 3", image: "image"),
+];
